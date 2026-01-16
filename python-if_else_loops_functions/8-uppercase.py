07527c26fb6456a1361a0c1f322ca5bfa13f3213
Hamsa1913 <Hamsa12021@gmail.com>
2026-01-16 11:33:40 +0000

    Fix indentation and docstring for uppercase function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Print a string in uppercase followed by a new line."""
-    for c in str:
-        if 'a' <= c <= 'z':
-            print("{}".format(chr(ord(c) - 32)), end="")
+    result = ""
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            result += chr(ord(char) - 32)
         else:
-            print("{}".format(c), end="")
-    print()
+            result += char
+    print("{}".format(result))
