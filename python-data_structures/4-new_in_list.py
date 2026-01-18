135593a0ac21129170045ac21fdb8ef4cd8c3058
Hamsa1913 <Hamsa12021@gmail.com>
2026-01-18 07:50:46 +0000

    Fix new_in_list function (remove circular import)

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-new_in_list = __import__('4-new_in_list').new_in_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]  # نسخ القائمة
+    if idx < 0 or idx >= len(new_list):
+        return new_list
+    new_list[idx] = element
+    return new_list
