4f4d55108057e954898f70a4dd54efe7937ab058
Hamsa1913 <Hamsa12021@gmail.com>
2026-01-18 07:47:00 +0000

    Fix print_reversed_list_integer using str.format()

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
-
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integer(my_list)
+def print_reversed_list_integer(my_list=[]):
+    for i in my_list[::-1]:
+        print("{:d}".format(i))
