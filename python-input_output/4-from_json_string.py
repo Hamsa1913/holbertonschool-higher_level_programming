84d09de46f083de9907ee4c9c9378f3778d4b081
“Hamsa1913” <Hamsa12021@gmail.com>
2026-02-02 07:25:54 +0000

    Add from_json_string function with PEP8-safe comment

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Module 4-from_json_string
-Contains function from_json_string that returns a Python object from a JSON string.
-"""
+"""Converts a JSON string to a Python object."""
 
 import json
 
-
+# Converts a JSON string to a Python object
+# Returns a list or dict depending on the input
 def from_json_string(my_str):
-    """Return the Python object represented by a JSON string."""
     return json.loads(my_str)
