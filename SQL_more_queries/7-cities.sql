e39e2a3831e823801ed119b47fcef51446d76f0a
Hamsa1913 <Hamsa12021@gmail.com>
2026-02-26 22:20:29 +0300

    Update 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,13 @@
--- Creates the database hbtn_0d_usa and the table states on your MySQL server.
+-- 7-cities.sql
+-- Creates database hbtn_0d_usa and table cities with foreign key to states
+-- cities: id (PK), state_id (FK to states.id), name
+-- If database or table already exists, script should not fail
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
